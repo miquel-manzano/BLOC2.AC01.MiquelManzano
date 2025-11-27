0c8f90c4f25fafa4887a96a27c7fa7e2386e28f4
miquel-manzano <miquel.manzano.7e6@itb.cat>
2025-11-27 10:25:53 +0100

    feat: Exercise one done.

--- BLOC2.AC01.DissenyModular/Program.cs
+++ BLOC2.AC01.DissenyModular/Program.cs
@@ -2,6 +2,13 @@
 {
     public static void Main()
     {
-        Console.WriteLine("Hello, World!");
+        int myNumber = 5, maxRange = 10, minRange = 4;
+        Console.WriteLine($"The number: {myNumber} is between {minRange} and {maxRange}??");
+        Console.WriteLine(isInRange(myNumber, minRange, maxRange) ? $"Yes, is between {minRange} and {maxRange}" : $"No, is not between {minRange} and {maxRange}");
+    }
+
+    public static bool isInRange(int number, int min, int max)
+    {
+        return number >= min && number <= max;
     }
 }
